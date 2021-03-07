b09442bf5b5ad3b39579eaa543fdf16b30c6960b
Eric Dobbs <eric@dobbse.net>
2021-03-06 17:09:49 -0700

    change traefik version & use ./.wiki

--- examples/friends/install.sh
+++ examples/friends/install.sh
@@ -1,3 +1,4 @@
 apk --update add gettext
+chown -R node:node .wiki
 su node sh -c "/usr/bin/envsubst < config/config.json > .wiki/config.json"
 su node sh -c "/usr/bin/envsubst < config/config.owner.json > .wiki/config.owner.json"
